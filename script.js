422c82a53d8268297ffc42e16505f1eeb45a50fe
Isaac Gabriel <isaacgbr7@gmail.com>
2026-06-09 12:45:22 -0300

    Atualizando script.js

--- script.js
+++ script.js
@@ -1,1 +1,55 @@
-console.log("teste");
+const dataFinal = new Date("2026-06-09T18:20:00");
+
+const timer = document.getElementById("timer");
+
+function atualizar(){
+
+const agora = new Date();
+
+const diferenca = dataFinal - agora;
+
+if(diferenca <= 0){
+
+document.getElementById("countdown").style.display = "none";
+
+document.getElementById("senha").style.display = "block";
+
+clearInterval(intervalo);
+
+return;
+}
+
+const horas = Math.floor(diferenca / 1000 / 60 / 60);
+
+const minutos = Math.floor((diferenca / 1000 / 60) % 60);
+
+const segundos = Math.floor((diferenca / 1000) % 60);
+
+timer.innerHTML =
+`${horas}h ${minutos}m ${segundos}s`;
+
+}
+
+const intervalo = setInterval(atualizar,1000);
+
+atualizar();
+
+function verificarSenha(){
+
+const resposta =
+document.getElementById("resposta").value.trim();
+
+if(resposta === "05/10/2026"){
+
+document.getElementById("senha").style.display = "none";
+
+document.getElementById("surpresa").style.display = "block";
+
+}else{
+
+document.getElementById("erro").innerHTML =
+"Resposta incorreta ❤️";
+
+}
+
+}
